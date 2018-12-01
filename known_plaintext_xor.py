a01765a5bacba96d036ff08444c4534fc2d9d799
rajell <34255376+rajell@users.noreply.github.com>
2018-12-01 16:31:17 +0000

    Update known_plaintext_xor.py

--- known_plaintext_xor.py
+++ known_plaintext_xor.py
@@ -3,7 +3,7 @@ import time,re
 
 def find_key():
 	key=""
-	HEADER_FILE="\x24\x2c\x9a\xe3\x62\x6e\x66\x61\x6c\x6c\x53\x6e\x66\x61"  # le header de notre fichier bmp encrypt
+	HEADER_FILE="\x24\x2c\x9a\xe3\x62\x6e\x66\x61\x6c\x6c\x53\x6e\x66\x61"  # le header de notre fichier bmp encrypte
 	HEADER_AVANT_ENC="\x42\x4d\xF6\x8F\x07\x00\x00\x00\x00\x00\x36\x00\x00\x00" # c'est le header du fichier avant encryption='BM'+taille+00+offset
 	for a,b in zip(HEADER_AVANT_ENC,HEADER_FILE):
 		key+=chr(ord(a)^ord(b))
@@ -12,7 +12,7 @@ def find_key():
 key=find_key()[:6]  #return fallenfallenfa
 cipher=""
 i=0
-for line in open('image.bmp'):
+for line in open('ch3.bmp'):
 	for x in line:
 		cipher+=chr(ord(x)^ord(key[i%6]))
 		i+=1
